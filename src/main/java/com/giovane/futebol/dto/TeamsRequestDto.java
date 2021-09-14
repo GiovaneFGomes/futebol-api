4ef7fe892283f7c3ee49c2104c87d00fb5b075a1
Giovane <giovanefernandes35@gmail.com>
2021-09-14 12:42:28 -0300

    removing annotation

--- src/main/java/com/giovane/futebol/dto/TeamsRequestDto.java
+++ src/main/java/com/giovane/futebol/dto/TeamsRequestDto.java
@@ -1,12 +1,11 @@
 package com.giovane.futebol.dto;
 
-import com.giovane.futebol.annotation.EliminateSpace;
 import com.giovane.futebol.model.Teams;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @AllArgsConstructor
@@ -16,20 +15,18 @@ public class TeamsRequestDto {
 
     private Integer id;
 
+    @NotBlank
     @Size(min = 3, max = 40, message = "min 3 and max 40 characters")
-    @NotNull
-    @EliminateSpace(message = "must not contain whitespace")
     private String name;
 
+    @NotBlank
     @Size(min = 3, max = 50, message = "min 3 and max 50 characters")
-    @NotNull
-    @EliminateSpace(message = "must not contain whitespace")
     private String stadium;
 
+    @NotBlank
     @Size(min = 5, max = 50, message = "min 5 and max 50 characters")
-    @NotNull
-    @EliminateSpace(message = "must not contain whitespace")
     private String country;
+
 
     // construtor que recebe dados da model
     public TeamsRequestDto(Teams teams) {
@@ -39,17 +36,16 @@ public class TeamsRequestDto {
         this.country = teams.getCountry();
     }
 
+    public void setName(String name) {
+        this.name = name.strip();
+    }
 
-//    public void setName(String name) {
-//        this.name = name.strip();
-//    }
-//
-//    public void setStadium(String stadium) {
-//        this.stadium = stadium.strip();
-//    }
-//
-//    public void setCountry(String country) {
-//        this.country = country.strip();
-//    }
+    public void setStadium(String stadium) {
+        this.stadium = stadium.strip();
+    }
+
+    public void setCountry(String country) {
+        this.country = country.strip();
+    }
 
 }
--- src/main/java/com/giovane/futebol/model/Teams.java
+++ src/main/java/com/giovane/futebol/model/Teams.java
@@ -1,23 +1,14 @@
 package com.giovane.futebol.model;
 
 import lombok.*;
-import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class Teams {
-
     private Integer id;
-
-    @Size(min = 3, max = 40, message = "min 3 and max 40 characters")
     private String name;
-
-    @Size(min = 3, max = 50, message = "min 3 and max 50 characters")
     private String stadium;
-
-    @Size(min = 5, max = 50, message = "min 5 and max 50 characters")
     private String country;
-
 }
